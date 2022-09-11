08f1f53a601bc74dee3f68d5189798c58717987a
Dan Mironenko <dmironenko@tempo.fit>
2022-09-11 22:05:08 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "src/**/*.elm",
+    "src/**/*.css",
+    "public/index.html",
+    "public/index.css",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
